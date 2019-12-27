c255c2889761d31a005b6d8c71ee2e5c7093e15a
szaizen <zaizen.saki@gmail.com>
2019-12-27 16:49:23 +0900

    add slider

--- pages/index.vue
+++ pages/index.vue
@@ -2,29 +2,22 @@
   <div class="page-index">
     <section class="about">
       <h2 class="section-title">
-        夏目漱石
+        My photo
       </h2>
-      <ArticleBody>
-        <img src="~/assets/images/portrait.png" alt="夏目漱石">
-        <p>日本の小説家、評論家、英文学者。本名、夏目 金之助（なつめ きんのすけ）。江戸の牛込馬場下横町（現在の東京都新宿区喜久井町）出身。俳号は愚陀仏。詳細は<a href="<https://ja.wikipedia.org/wiki/夏目漱石>" target="_blank">ウィキペディア</a>を。</p>
-      </ArticleBody>
-    </section>
-
-    <section class="slider">
-      <Swiper />
+      <Swiper></Swiper>
     </section>
 
     <section class="works">
       <h2 class="section-title">
-        代表作
+        プロフィール
       </h2>
       <ArticleBody>
-        <p>『吾輩は猫である』（1905年）がデビュー作である。</p>
+        <p>福岡生まれ、福岡育ち。好きなものは以下。</p>
         <ul>
-          <li>『吾輩は猫である』（1905年）</li>
-          <li>『坊っちゃん』（1906年）</li>
-          <li>『草枕』（1906年）</li>
-          <li>『三四郎』（1908年）</li>
+          <li>幕末志士</li>
+          <li>ゲーム</li>
+          <li>音楽</li>
+          <li>開発</li>
         </ul>
       </ArticleBody>
     </section>
